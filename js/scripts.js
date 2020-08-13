df3a5b69a999de0035ecf3b0f6754727adc93ccb
Amanda Otero <mandystar018@gmail.com>
2020-08-13 10:18:28 -0700

    add functionality to the scritpts.js
    co-authored-by: Donny Mays <dtmays2@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+const animal = $("input:radio[name=animal]:checked").val();
+  
+
+if (catch.checked) {
+    $("#cat").show();
+  // } else if (animal === "jirafa") {
+  //   $("#")
+  // }
+});
